0d11ea62554048da773a31298322aff7d9fd1737
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2023-11-27 10:56:30 +0530

    add new exercise

--- exercise/Data_Structure/collation.py
+++ exercise/Data_Structure/collation.py
@@ -30,3 +30,68 @@ sample_list.append(element)
 print("List after Adding element at last ", sample_list)
 
 # 3]Slice list into 3 equal chunks and reverse each chunk
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+print("Original list ", sample_list)
+
+length = len(sample_list)
+chunk_size = int(length / 3)
+start = 0
+end = chunk_size
+
+# run loop 3 times
+for i in range(3):
+    # get indexes
+    indexes = slice(start, end)
+    
+    # get chunk
+    list_chunk = sample_list[indexes]
+    print("Chunk ", i, list_chunk)
+    
+    # reverse chunk
+    print("After reversing it ", list(reversed(list_chunk)))
+
+    start = end
+    end += chunk_size
+
+#4] Count the occurrence of each element from a list
+sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+print("Original list ", sample_list)
+
+count_dict = dict()
+for item in sample_list:
+    if item in count_dict:
+        count_dict[item] += 1
+    else:
+        count_dict[item] = 1
+
+print("Printing count of each item  ", count_dict)
+
+# 5]Create a Python set such that it shows the element from both lists in a pair
+first_list = [2, 3, 4, 5, 6, 7, 8]
+print("First List ", first_list)
+
+second_list = [4, 9, 16, 25, 36, 49, 64]
+print("Second List ", second_list)
+
+result = zip(first_list, second_list)
+result_set = set(result)
+print(result_set)
+
+#6] Remove duplicates from a list and create a tuple and find the minimum and maximum number
+sample_list = [87, 52, 44, 53, 54, 87, 52, 53]
+
+print("Original list", sample_list)
+
+sample_list = list(set(sample_list))
+print("unique list", sample_list)
+
+t = tuple(sample_list)
+print("tuple ", t)
+
+print("Minimum number is: ", min(t))
+print("Maximum number is: ", max(t))
+
+
+
+
+
---
+++ exercise/Decorators/Decorators.py
@@ -0,0 +1,28 @@
+# syntax for decorators
+
+# @decorator_function()
+def my_function():
+    pass
+# The @decorator_function notation is just a shorthand for the following code:
+def my_function():
+    pass
+# my_function = decorator_function(my_function)
+
+# One common use of decorators is to add logging to a function. 
+# For example, you could use a decorator to log the arguments and return value of a function each time it is called:
+
+import logging
+
+def log_function_call(func):
+    def decorated(*args, **kwargs):
+        logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"{func.__name__} returned {result}")
+        return result
+    return decorated
+
+@log_function_call
+def my_function(a, b):
+    return a + b
+
+
