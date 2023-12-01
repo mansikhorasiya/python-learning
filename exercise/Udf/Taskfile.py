16c91d84ba8e44d6ae50b51cb8a07c3e35372012
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2023-12-01 10:46:08 +0530

    add UDF taskfile

---
+++ exercise/Udf/Taskfile.py
@@ -0,0 +1,21 @@
+# 1]word wise reverse
+# def fn1(s):
+#     d=s.split(" ")
+#     print(d)
+#     for i in d:
+#         print(i[::-1],end=' ')
+# n=input('Enter the string:')
+# fn1(n)
+
+# 2]two charachters interchange
+print()
+def fun1(s):
+    d=s.split(" ")
+    print(d)
+    b= len(d)
+    for i in d:
+        for j in range(0,len(i),2):
+            print(i[j:j +2][::-1],end='')
+        print(end=' ')
+n=(input("Enter the string:"))
+fun1(n)
