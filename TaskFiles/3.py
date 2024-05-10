84867f283d6d14df69c5c4d8cbaf54c2312e05f3
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-05-10 18:03:05 +0530

    Find Armstrong number and second largest number of list

--- TaskFiles/3.py
+++ TaskFiles/3.py
@@ -1,13 +1,45 @@
-num=int(input('Enter any number:'))
-sum=0
-temp=num
+# num=int(input('Enter any number:'))
+# sum=0
+# temp=num
 
-while temp>0:
-    digit = temp % 10
-    cube = digit ** 3
-    sum = sum+cube
-    temp //= 10
-if sum == num:
-    print('it is an Armstrong Number')
+# while temp>0:
+#     digit = temp % 10
+#     cube = digit ** 3
+#     sum = sum+cube
+#     temp //= 10
+# if sum == num:
+#     print('it is an Armstrong Number')
+# else:
+#     print('it is not an Armstrong Number')
+# >>>>>>>>>>>>>>>>>>>>>>>>
+
+class ArmstrongChecker:
+    def __init__(self, num):
+        self.num = num
+    
+    def check_armstrong(self):
+        sum_of_cubes = 0
+        temp = self.num
+
+        while temp > 0:
+            digit = temp % 10
+            cube = digit ** 3
+            sum_of_cubes += cube
+            temp //= 10
+
+        if sum_of_cubes == self.num:
+            return True
+        else:
+            return False
+
+# Input number from the user
+num = int(input('Enter any number: '))
+
+# Create an instance of ArmstrongChecker
+armstrong_checker = ArmstrongChecker(num)
+
+# Check if the number is an Armstrong number
+if armstrong_checker.check_armstrong():
+    print('It is an Armstrong Number')
 else:
-    print('it is not an Armstrong Number')
+    print('It is not an Armstrong Number')
--- TaskFiles/4.py
+++ TaskFiles/4.py
@@ -1,16 +1,38 @@
-# Example list of random numbers
+# # Example list of random numbers
 numbers = [5, 8, 2, 1, 6, 5, 7, 6]
 
-def find_second_largest(numbers):
-    # Find the maximum number
-    max_number = max(numbers)
-    # Remove the maximum number from the list
-    numbers.remove(max_number)
-    # Find the maximum number again (now it will be the second largest)
-    second_largest = max(numbers)
-    return second_largest
+# def find_second_largest(numbers):
+#     # Find the maximum number
+#     max_number = max(numbers)
+#     # Remove the maximum number from the list
+#     numbers.remove(max_number)
+#     # Find the maximum number again (now it will be the second largest)
+#     second_largest = max(numbers)
+#     return second_largest
 
+
+# # Find the second largest number
+# second_largest = find_second_largest(numbers)
+# print("The second largest number is:", second_largest)
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+class SecondLargestFinder:
+    def __init__(self, numbers):
+        self.numbers = numbers
+    
+    def find_second_largest(self):
+        # Remove duplicates and sort the list
+        sorted_numbers = sorted(set(self.numbers))
+        
+        # Check if there are at least two unique numbers in the list
+        if len(sorted_numbers) < 2:
+            return "There is no second largest number."
+        else:
+            return sorted_numbers[-2]
+
+# Create an instance of SecondLargestFinder
+finder = SecondLargestFinder(numbers)
 
 # Find the second largest number
-second_largest = find_second_largest(numbers)
+second_largest = finder.find_second_largest()
 print("The second largest number is:", second_largest)
+
