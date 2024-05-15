99d629c354c352d311e159fb853eb6b249a7c6a8
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-05-15 21:35:28 +0530

    pallidrome or/not

---
+++ preperations/palindromnum.py
@@ -0,0 +1,9 @@
+n= (input("Enter any string:"))
+
+def is_pallindrome(s):
+    return s == s[::-1]
+
+if is_pallindrome(n):
+    print("The string is a palindrome")
+else:
+    print("The string is  not palindrome")
